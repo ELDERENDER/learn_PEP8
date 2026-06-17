1ba29db1d8b1a07a3cf629bab9d132d9e8ad2435
ELDERENDER <mazurkevic75@gmail.com>
2026-06-18 00:10:20 +0700

    Add files via upload

---
+++ task_3.py
@@ -0,0 +1,26 @@
+def very_long_function_name_with_many_parameters(
+        param1, param2, param3, param4, param5, param6
+):
+    total = param1 + param2 + param3 + param4 + param5 + param6
+    return total * 1.18
+
+
+def func1():
+    x = 1
+    y = 2
+    return x + y
+
+
+def func2(a, b):
+    if a > b:
+        return a
+    else:
+        return b
+
+
+class Test:
+    def method1(self):
+        pass
+
+    def method2(self):
+        pass
